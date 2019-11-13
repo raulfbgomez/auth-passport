7308fe846af188383c65e15fc057c653302dadc0
Raul Gomez <raul_nouni@hotmail.com>
2019-11-12 23:24:03 -0600

    Implementando Sign-in

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const { config } = require('./config/index');
+const authApi = require('./routes/auth');
 const moviesApi = require('./routes/movies.js');
 const userMoviesApi = require('./routes/userMovies.js');
 
@@ -15,6 +16,7 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler.js');
 app.use(express.json());
 
 // routes
+authApi(app);
 moviesApi(app);
 userMoviesApi(app);
 
