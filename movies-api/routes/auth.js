49befc9cf7cc0db07e99729dcedf31d6055add0b
= <=>
2019-11-14 00:08:08 -0600

    Implementacion de sign-up

--- movies-api/routes/auth.js
+++ movies-api/routes/auth.js
@@ -3,6 +3,9 @@ const passport = require('passport');
 const boom = require('@hapi/boom');
 const jwt = require('jsonwebtoken');
 const ApiKeysService = require('../services/apiKeys');
+const UsersService = require('../services/users');
+const validationHandler = require('../utils/middleware/validationHandler');
+const { createUserSchema } = require('../utils/schemas/users');
 const { config } = require('../config');
 
 // Basic strategy
@@ -12,6 +15,7 @@ function authApi(app) {
   const router = express.Router();
   app.use('/api/auth', router);
   const apiKeysService = new ApiKeysService();
+  const usersService = new UsersService();
 
   router.post('/sign-in', async function(req, res, next) {
     const { apiKeyToken } = req.body;
@@ -51,6 +55,19 @@ function authApi(app) {
         next(error);
       }
     })(req, res, next);
+  });
+
+  router.post('/sign-up', validationHandler(createUserSchema), async function(req, res, next) {
+    const {body: user} = req;
+    try {
+      const createdUserId = await usersService.createUser({ user });
+      res.status(201).json({
+        data: createdUserId,
+        message: 'user created'
+      })
+    } catch (error) {
+      next(error);
+    }
   })
 }
 
